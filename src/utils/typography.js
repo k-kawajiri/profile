497120de54a36ff068fe2a85f597dca94f8bf6e3
k-kawajiri <kawanojieee@gmail.com>
2020-02-03 21:55:10 +0900

    [update]font-familyを変える

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,18 +1,16 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
-
+const fontFamily = [
+  "BlinkMacSystemFont",
+  "-apple-system",
+  "YuGothic",
+  "Meiryo",
+  "serif",
+]
+const typography = new Typography({
+  headerFontFamily: fontFamily,
+  bodyFontFamily: fontFamily,
+})
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
   typography.injectStyles()
