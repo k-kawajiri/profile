4128fef50643e0109b55c93ae7906b12c0fdcae2
k-kawajiri <kawanojieee@gmail.com>
2020-02-03 23:02:55 +0900

    [update]試行錯誤

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,42 +1,39 @@
 import React from "react"
-import { Link, useStaticQuery } from "gatsby"
 
-export default aa => {
-  console.log(aa)
-  const mark = useStaticQuery(
-    graphql`
-      query TestQuery{
-        markdownRemark(frontmatter: { path: { eq: "/test" } }) {
-          html
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            path
-            title
-          }
-        }
-      }
-    `
-  )
-  console.log(mark, aa)
-  return (
-    <div style={{ color: `teal` }}>
-      <h1>About Gatsby</h1>
-      <p>Such wow. Very React.</p>
-      <Link to="/">Home</Link>
-      <div dangerouslySetInnerHTML={{ __html: mark.markdownRemark.html }} />
-    </div>
-  )
+class Profile extends React.Component {
+  render() {
+    const mark = this.props.data.markdownRemark
+    // const mark = useStaticQuery(
+    //   graphql`
+    //     query TestQuery {
+    //       markdownRemark(frontmatter: { path: { eq: "/profile" } }) {
+    //         html
+    //         frontmatter {
+    //           date(formatString: "MMMM DD, YYYY")
+    //           path
+    //           title
+    //         }
+    //       }
+    //     }
+    //   `
+    // )
+    return (
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: mark.html }} />
+      </div>
+    )
+  }
 }
-
-// export const pageQuery = graphql`
-//   query HogeQuery{
-//     markdownRemark(frontmatter: { path: { eq: "/introduction" } }) {
-//       html
-//       frontmatter {
-//         date(formatString: "MMMM DD, YYYY")
-//         path
-//         title
-//       }
-//     }
-//   }
-// `
+export default Profile
+export const pageQuery = graphql`
+  query HogeQuery{
+    markdownRemark(frontmatter: { path: { eq: "/profile" } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`
